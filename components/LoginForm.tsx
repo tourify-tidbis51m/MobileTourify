d0b82ec39c7da16f7e9963b8d6811335c5aace78
TheSixDecadence <DelosSantosJ.10.3A@gmail.com>
2024-06-14 14:19:53 -0600

    Login a medias

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import { Animated, TextInput, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const LoginForm = () => {
 
@@ -21,9 +23,10 @@ const LoginForm = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.label}>Enter your email:</Text>
+            <Text style={styles.labelTitle}>TOURIFY</Text>
+            <Text style={styles.label}>Ingresa tu correo:</Text>
             <TextInput style={styles.input} />
-            <Text style={styles.label}>Enter your password:</Text>
+            <Text style={styles.label}>Ingresa tu contraseña:</Text>
             <TextInput style={styles.input} secureTextEntry />
             <TouchableOpacity
                 onPressIn={handlePressIn}
@@ -31,7 +34,16 @@ const LoginForm = () => {
                 style={styles.buttonContainer}
             >
                 <Animated.View style={[styles.button, { transform: [{ scale: scaleAnim }] }]}>
-                    <Text style={styles.buttonText}>LOGIN</Text>
+                    <Text style={styles.buttonText}>INICIAR SESIÓN</Text>
+                </Animated.View>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPressIn={handlePressIn}
+                onPressOut={handlePressOut}
+                style={styles.buttonContainer}
+            >
+                <Animated.View style={[styles.buttonRegister, { transform: [{ scale: scaleAnim }] }]}>
+                    <Text style={styles.buttonText}>¿Todavía no tienes una cuenta?</Text>
                 </Animated.View>
             </TouchableOpacity>
         </View>
@@ -40,7 +52,7 @@ const LoginForm = () => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        backgroundColor: '#F4E8FF',
         padding: 20,
         borderRadius: 10,
         color: 'black',
@@ -55,32 +67,31 @@ const styles = StyleSheet.create({
     },
     label: {
         fontSize: 23,
-        marginBottom: 5,
+        marginBottom: -20,
         alignItems: 'center',
         justifyContent: 'center',
         display: 'flex',
         flexDirection: 'column',
         fontFamily: 'system-ui',
-        fontStyle: 'italic',
-
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        marginBottom: 20,
-        alignSelf: 'center',
+        fontWeight: 'bold',
+        top: -30
     },
     input: {
         height: 40,
         borderColor: 'gray',
-        borderWidth: 0,
-        marginBottom: 10,
+        marginBottom: 40,
         paddingHorizontal: 10,
-        borderBottomWidth: 2,
-
+        borderBottomWidth: 3,
     },
     button: {
         backgroundColor: 'blue',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonRegister: {
+        backgroundColor: '#3B05FF',
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
@@ -95,7 +106,18 @@ const styles = StyleSheet.create({
     buttonContainer: {
         borderRadius: 5,
         overflow: 'hidden',
+        top: -22
     },
+    labelTitle: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontFamily: 'Sans-Serif, Lucida Sans',
+        fontSize: 50,
+        justifyContent: 'center',
+        display: 'flex',
+        top: -40,
+        bottom: 10
+    }
 });
 
 export default LoginForm;
