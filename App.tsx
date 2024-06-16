3da7912541008d6f8bdaabc3d266cfe8afc95191
XartCircleX <bryanalexisdelgadolopez1@gmail.com>
2024-06-15 21:32:56 -0600

    se me olvido el navigate

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 //import LoginScreen from './pages/LoginScreen';
 import AppNavigator from './routes/AppNavigator';
-import MainMenu from './pages/MainMenuScreen';
 export default function App() {
   return(
-      <MainMenu />
+      <AppNavigator />
   ) 
 }
 
